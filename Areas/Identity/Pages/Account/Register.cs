3fc69cff2fb8b32926a852bad729648ef65f2f4f
Cole Crain <cwcrain2000@outlook.com>
2020-12-03 16:50:46 -0500

    Add project files.

---
+++ Areas/Identity/Pages/Account/Register.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Areas.Identity.Pages.Account
+{
+    public class Register
+    {
+    }
+}
